e7f54d3a0a93a5e77e31f6cd4175d1bfd8afd845
phamdaiyb98 <44748066+phamdaiyb98@users.noreply.github.com>
2019-06-11 16:56:24 +0700

    commit002

---
+++ learning-open-source-summer-2019/src/main/java/com/opensource/LearningOpenSourceSummer2019Application.java
@@ -0,0 +1,13 @@
+package com.opensource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOpenSourceSummer2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningOpenSourceSummer2019Application.class, args);
+	}
+
+}
