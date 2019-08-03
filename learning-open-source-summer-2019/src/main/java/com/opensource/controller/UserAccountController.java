e6c59676cf84237082c3817bb84e70c1cd94c48a
QuocCuong0701 <quoc.cuong.yb.dhtb@gmail.com>
2019-08-03 09:02:38 +0700

    i

--- learning-open-source-summer-2019/src/main/java/com/opensource/controller/UserAccountController.java
+++ learning-open-source-summer-2019/src/main/java/com/opensource/controller/UserAccountController.java
@@ -1,15 +1,23 @@
 package com.opensource.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.opensource.service.UserService;
 
 @Controller
 @RequestMapping("/admin")
 public class UserAccountController {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/user-account")
-	String userAccount() {
+	String userAccount(Model model) {
+		model.addAttribute("customers", userService.customer());
 		return "admin/user-account";
 	}
 	
--- learning-open-source-summer-2019/src/main/java/com/opensource/service/UserService.java
+++ learning-open-source-summer-2019/src/main/java/com/opensource/service/UserService.java
@@ -1,6 +1,7 @@
 package com.opensource.service;
 
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -64,5 +65,7 @@ public class UserService {
 		
 	}
 
-
+	public List<User> customer(){
+		return userRepository.customer();
+	}
 }
